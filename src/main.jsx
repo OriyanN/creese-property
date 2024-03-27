32ccf390ff565aa08ff362307825fee827cc40a0
OriyanPropellProperty <oriyan@propellproperty.com.au>
2024-03-27 17:00:37 +1000

    main updated

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import './index.css'
 
-import HomePage from"./pages/HomePage.jsx";
-import AboutPage from"./pages/AboutPage.jsx";
-import ContactPage from"./pages/ContactPage.jsx";
+import HomePage from "./pages/HomePage.jsx";
+import AboutPage from "./pages/AboutPage.jsx";
+import LocationsPage from "./pages/LocationsPage.jsx";
+import ContactPage from "./pages/ContactPage.jsx";
 
 
 import NavBar from './components/NavBar.jsx';
@@ -17,6 +18,7 @@ const router = createBrowserRouter([
     children: [
       { path:"/", element: <HomePage /> },
       { path: "/about", element: <AboutPage /> },
+      { path: "/locations", element: <LocationsPage /> },
       { path: "/contact", element: <ContactPage /> },
       // { path: "/project/:id", element: <ProjectPage /> },
       // { path: "/project/:id/update", element: <UpdateProject /> },
