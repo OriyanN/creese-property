ace1b0377c1864ce0860b16bbc3283059f2311e0
OriyanPropellProperty <oriyan@propellproperty.com.au>
2024-03-27 11:35:30 +1000

    set up

---
+++ src/main.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import './index.css'
+
+import HomePage from"./pages/HomePage.jsx";
+import AboutPage from"./pages/AboutPage.jsx";
+import ContactPage from"./pages/ContactPage.jsx";
+
+
+import NavBar from './components/NavBar.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <NavBar />,
+    children: [
+      { path:"/", element: <HomePage /> },
+      { path: "/about", element: <AboutPage /> },
+      { path: "/contact", element: <ContactPage /> },
+      // { path: "/project/:id", element: <ProjectPage /> },
+      // { path: "/project/:id/update", element: <UpdateProject /> },
+      // { path: "/profile", element: <ProfilePage /> },
+      // { path: "/create-project", element: <CreateProject /> },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+     <RouterProvider router={router} />
+  </React.StrictMode>,
+)
