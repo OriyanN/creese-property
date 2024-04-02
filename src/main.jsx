d3604b0864bc31b684a687324fe415da423aa00b
OriyanPropellProperty <oriyan@propellproperty.com.au>
2024-04-03 08:57:33 +1000

    main updated

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,10 @@ import './index.css'
 import HomePage from "./pages/HomePage.jsx";
 import AboutPage from "./pages/AboutPage.jsx";
 import GoldCoastPage from './pages/GoldCoastPage.jsx';
+import LoganPage from './pages/LoganPage.jsx';
+import IpswichPage from './pages/IpswichPage.jsx';
+import BrisbanePage from './pages/BrisbanePage.jsx';
+import SunshineCoastPage from './pages/SunshineCoastPage.jsx';
 import ContactPage from "./pages/ContactPage.jsx";
 
 
@@ -19,6 +23,10 @@ const router = createBrowserRouter([
       { path:"/", element: <HomePage /> },
       { path: "/about", element: <AboutPage /> },
       { path: "/locations/gold-coast", element: <GoldCoastPage /> },
+      { path: "/locations/logan", element: <LoganPage /> },
+      { path: "/locations/ipswich", element: <IpswichPage /> },
+      { path: "/locations/brisbane", element: <BrisbanePage /> },
+      { path: "/locations/sunshine-coast", element: <SunshineCoastPage /> },
       { path: "/contact", element: <ContactPage /> },
       // { path: "/project/:id", element: <ProjectPage /> },
       // { path: "/project/:id/update", element: <UpdateProject /> },
