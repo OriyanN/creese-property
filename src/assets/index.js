87631348c2c5545e0c5ea111460cb7a4c9000e5d
OriyanPropellProperty <oriyan@propellproperty.com.au>
2024-04-05 10:58:27 +1000

    assets index.js

---
+++ src/assets/index.js
@@ -0,0 +1,13 @@
+import image0_Creese_Property_Favicon_White_png from './Creese Property Favicon White.png';
+import image1_Creese_Property_Logo__White_White_png from './Creese Property Logo- White White.png';
+import image2_Creese_Property_Logo_Beige_png from './Creese Property Logo-Beige.png';
+import image3_sunshine_coast_png from './sunshine-coast.png';
+import image4_sunshine_coast2_png from './sunshine-coast2.png';
+
+export {
+  image0_Creese_Property_Favicon_White_png,
+  image1_Creese_Property_Logo__White_White_png,
+  image2_Creese_Property_Logo_Beige_png,
+  image3_sunshine_coast_png,
+  image4_sunshine_coast2_png
+};
