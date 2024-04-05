70fe50709b80df926fe8d7433d2a6735ca5c4621
OriyanPropellProperty <oriyan@propellproperty.com.au>
2024-04-05 14:30:27 +1000

    footer adjutsed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,6 +3,13 @@ import { Link, Outlet, useLocation } from "react-router-dom";
 
 import logo from "../assets/Creese Property Logo- White White.png";
 
+import facebook_blue from "../assets/icons8-facebook-50.png";
+import facebook_white from "../assets/icons8-facebook-50 copy.png";
+import instagram_blue from "../assets/icons8-instagram-50.png";
+import instagram_white from "../assets/icons8-instagram-50 (1).png";
+import linkedin_blue from "../assets/icons8-linkedin-50.png";
+import linkedin_white from "../assets/icons8-linkedin-50 (1).png";
+
 import './Footer.css';
 
 const Footer = () => {
@@ -29,21 +36,21 @@ const Footer = () => {
           <ul>
             <li className="facebook-icon">
               <Link href="">
-                <img className="social-media-blue" width="30" height="30" src="src/assets/icons8-facebook-50.png" alt="facebook-new--v3-icon8"/>
-                <img className="social-media-white" width="30" height="30" src="src/assets/icons8-facebook-50 copy.png" alt="facebook-new--v3-icon8"/>
+                <img className="social-media-blue" width="30" height="30" src={facebook_blue} alt="facebook-new--v3-icon8"/>
+                <img className="social-media-white" width="30" height="30" src={facebook_white} alt="facebook-new--v3-icon8"/>
               </Link>
             </li>
             <li className="instagram-icon">
               <Link href="">
-                  <img className="social-media-blue" width="30" height="30" src="src/assets/icons8-instagram-50.png" alt="instagram-new--v3-icon8"/>
-                  <img className="social-media-white" width="30" height="30" src="src/assets/icons8-instagram-50 (1).png" alt="instagram-new--v3-icon8"/>
+                  <img className="social-media-blue" width="30" height="30" src={instagram_blue} alt="instagram-new--v3-icon8"/>
+                  <img className="social-media-white" width="30" height="30" src={instagram_white} alt="instagram-new--v3-icon8"/>
               </Link>
             </li>
             <li className="linkedin-icon">
               <div className="linkedin-icon">
                 <Link href="">
-                  <img className="social-media-blue" width="30" height="30" src="src/assets/icons8-linkedin-50.png" alt="linkedin-2--v2-icon8"/>
-                  <img className="social-media-white" width="30" height="30" src="src/assets/icons8-linkedin-50 (1).png" alt="linkedin-2--v2-icon8"/>
+                  <img className="social-media-blue" width="30" height="30" src={linkedin_blue} alt="linkedin-2--v2-icon8"/>
+                  <img className="social-media-white" width="30" height="30" src={linkedin_white} alt="linkedin-2--v2-icon8"/>
                 </Link>
               </div>
             </li>
