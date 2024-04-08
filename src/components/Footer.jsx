f500dff3c761f38ffd46a9040115d9522263ab77
OriyanPropellProperty <oriyan@propellproperty.com.au>
2024-04-08 13:54:13 +1000

    footer updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, Outlet, useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 import logo from "../assets/Creese Property Logo- White White.png";
 
