f16d90275400518b8c6a986ad9b98e9c1e5c3c6d
OriyanPropellProperty <oriyan@propellproperty.com.au>
2024-04-10 10:16:35 +1000

    footer updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,63 +1,86 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 
+import ScrollToTop from './ScrollToTop';
+
 import logo from "../assets/Creese Property Logo- White White.png";
 
-import facebook_blue from "../assets/icons8-facebook-50.png";
-import facebook_white from "../assets/icons8-facebook-50 copy.png";
-import instagram_blue from "../assets/icons8-instagram-50.png";
-import instagram_white from "../assets/icons8-instagram-50 (1).png";
-import linkedin_blue from "../assets/icons8-linkedin-50.png";
-import linkedin_white from "../assets/icons8-linkedin-50 (1).png";
+import facebook_blue from "../assets/facebook-blue.png";
+import facebook_white from "../assets/facebook-white.png";
+import instagram_blue from "../assets/icons8-instagram-48 (1).png";
+import instagram_white from "../assets/icons8-instagram-48.png";
+import linkedin_blue from "../assets/icons8-linkedin-48.png";
+import linkedin_white from "../assets/icons8-linkedin-48 (1).png";
 
 import './Footer.css';
 
 const Footer = () => {
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth", // Optional: Adds a smooth scrolling effect
+    });
+  };
+    
   return (
-    <footer className="footer">
-      <Link to="/" className="logo"><img src={logo} alt="Logo" /></Link>
-      <div className="footer-links">
-        <Link to="/" >
-          Home
-        </Link>
-        <Link to="/about">
-          About
-        </Link>
-        <Link to="/contact" >
-          Contact
-        </Link>
-        <Link to="/privacy" >
-          Privacy
-        </Link>
-      </div>
-      <div className="footer-bottom">
-        <p>&copy; {new Date().getFullYear()} Creese Property. All Rights Reserved.</p>
+    <>
+      <ScrollToTop />
+      <footer className="footer">
+        <Link to="/" className="logo"><img src={logo} alt="Logo" /></Link>
         <div className="social-media">
           <ul>
             <li className="facebook-icon">
               <Link href="">
-                <img className="social-media-blue" width="30" height="30" src={facebook_blue} alt="facebook-new--v3-icon8"/>
-                <img className="social-media-white" width="30" height="30" src={facebook_white} alt="facebook-new--v3-icon8"/>
-              </Link>
-            </li>
-            <li className="instagram-icon">
-              <Link href="">
-                  <img className="social-media-blue" width="30" height="30" src={instagram_blue} alt="instagram-new--v3-icon8"/>
-                  <img className="social-media-white" width="30" height="30" src={instagram_white} alt="instagram-new--v3-icon8"/>
+                <img className="social-media-white" width="20" height="20" src={facebook_white} alt="facebook-new--v3-icon8"/>
+                <img className="social-media-blue" width="20" height="20" src={facebook_blue} alt="facebook-new--v3-icon8"/>
               </Link>
             </li>
             <li className="linkedin-icon">
               <div className="linkedin-icon">
                 <Link href="">
-                  <img className="social-media-blue" width="30" height="30" src={linkedin_blue} alt="linkedin-2--v2-icon8"/>
-                  <img className="social-media-white" width="30" height="30" src={linkedin_white} alt="linkedin-2--v2-icon8"/>
+                  <img className="social-media-white" width="20" height="20" src={linkedin_white} alt="linkedin-2--v2-icon8"/>
+                  <img className="social-media-blue" width="20" height="20" src={linkedin_blue} alt="linkedin-2--v2-icon8"/>
                 </Link>
               </div>
             </li>
+            <li className="instagram-icon">
+              <Link href="">
+                  <img className="social-media-white" width="20" height="20" src={instagram_white} alt="instagram-new--v3-icon8"/>
+                  <img className="social-media-blue" width="20" height="20" src={instagram_blue} alt="instagram-new--v3-icon8"/>
+              </Link>
+            </li>
           </ul> 
         </div>
-      </div>
-    </footer>
+        <div className="footer-links">
+          <Link to="/" onClick={scrollToTop}>
+            Home
+          </Link>
+          <Link to="/about" onClick={scrollToTop}>
+            About
+          </Link>
+          <Link to="/services" >
+            Services
+          </Link>
+          <Link to="/portfolio" onClick={scrollToTop}>
+            Portfolio
+          </Link>
+          <Link to="/contact" onClick={scrollToTop}>
+            Contact
+          </Link>
+        </div>
+        <div className="footer-bottom">
+          <p>&copy; {new Date().getFullYear()} Creese Property. All Rights Reserved.</p>
+          <div className="footer-bottom-links">
+            <Link to="/privacy" >
+              Privacy
+            </Link>
+            <Link to="/terms-conditions" >
+              Terms & Conditions
+            </Link>
+          </div>
+        </div>
+      </footer>
+    </>
   );
 };
 
