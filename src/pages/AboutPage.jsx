6bb708abe7dc52dcf5aca87b93d3b4684079aca1
OriyanPropellProperty <oriyan@propellproperty.com.au>
2024-04-11 14:01:17 +1000

    About Page partially complete

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -20,30 +20,11 @@ function AboutPage() {
                     <div className="about-text">
                         <h2>Lorem Ipsum</h2>
                         <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Fringilla est ullamcorper eget nulla facilisi. Volutpat lacus laoreet non curabitur. Aenean et tortor at risus viverra. In hendrerit gravida rutrum quisque non tellus orci ac auctor. Pellentesque dignissim enim sit amet venenatis urna cursus. Lacus vel facilisis volutpat est velit egestas dui id ornare. Faucibus vitae aliquet nec ullamcorper sit amet risus nullam eget. Fusce ut placerat orci nulla pellentesque dignissim enim sit amet. Potenti nullam ac tortor vitae purus faucibus.
-                            <br /><br />
-                            Eget magna fermentum iaculis eu non diam phasellus. Metus aliquam eleifend mi in nulla. Pellentesque dignissim enim sit amet venenatis urna cursus. Phasellus vestibulum lorem sed risus ultricies tristique nulla aliquet. Sem fringilla ut morbi tincidunt. Feugiat scelerisque varius morbi enim nunc faucibus a pellentesque sit. Ac orci phasellus egestas tellus rutrum tellus pellentesque eu tincidunt. Vitae sapien pellentesque habitant morbi tristique senectus et netus et. At risus viverra adipiscing at. In hac habitasse platea dictumst quisque sagittis. Ornare arcu dui vivamus arcu felis. At volutpat diam ut venenatis tellus in. Ac turpis egestas sed tempus urna et pharetra pharetra.
+                            Creese Property Pty Ltd emerges as a tribute to the enduring legacy of its founder's lineage, with Managing Director Lachlan Creese dedicating the company to the memory of his late father, David Creese. David was a pioneering figure in the real estate industry, having established Creese Property Consultants on the 15th of May 1981. His firm became a cornerstone in the Southern Tasmanian Commercial Management sector, extending its expertise to encompass residential and commercial development consultancy. Under David's visionary leadership, Creese Property Consultants became instrumental in the development of what is now known as Rosny Park, a key commercial hub on Hobart’s Eastern shore. David’s contributions to real estate also include the establishment of 'Creese Drive' in Richmond, Tasmania, a testament to his success in private residential development that resulted in the creation of over 100 additional lots, predominantly along Cosgrove Drive. It is in this vibrant community that Lachlan Creese spent his formative years, growing up in the family home and gaining firsthand experience in the construction industry as he accompanied his father to numerous sites. David Creese personally took part in the construction and sale of more than 30 houses, instilling in Lachlan a profound understanding and passion for the real estate sector.
+                            <br />
+                            <br />
+                            With this rich heritage as a foundation, Lachlan Creese, in partnership with Michael Pell, is poised to usher in a new era for the family's legacy with the inauguration of Creese Property Pty Ltd. This new venture marks a significant expansion of the Creese legacy, aiming to specialise in residential property management services across Southeast Queensland. Lachlan is enthusiastic about this opportunity to honour his father’s storied career and impact by steering Creese Property into new territories, ensuring that the legacy of David Creese continues to influence the Australian real estate landscape through innovative services and a commitment to excellence in property management.
                         </p>
-                    </div>
-                </div>
-                <div className="services-container">
-                    <h2>Services</h2>
-                    <div className="services">
-                        <div className="service">
-                            <img src="src/assets/1.png" height={100} alt="" />
-                            <h3>Tailored Solutions</h3>
-                            <p>We understand that every property owner and tenant has their unique needs. That’s why we offer customised management plans to suit your specific requirements.</p>
-                        </div>
-                        <div className="service">
-                            <img src="src/assets/2.png" height={100} alt="" />
-                            <h3>Expert Advice</h3>
-                            <p>Our team stays aware of the latest market trends and regulations to provide you with informed advice, helping you make the best decisions for your property.</p>
-                        </div>
-                        <div className="service">
-                            <img src="src/assets/3.png" height={100} alt="" />
-                            <h3>Community Focus</h3>
-                            <p>We’re not just managing properties, we’re building communities. Creese Property is committed to creating vibrant living spaces where residents can feel at home.</p>
-                        </div>
                     </div>
                 </div>
                 <div className="team-container">
@@ -72,6 +53,27 @@ function AboutPage() {
                                 <h4>Property Manager</h4>
                             </div>
                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Purus semper eget duis at tellus at urna condimentum mattis. Etiam non quam lacus suspendisse faucibus. Est ultricies integer quis auctor elit. Mauris commodo quis imperdiet massa tincidunt nunc pulvinar sapien et. Non quam lacus suspendisse faucibus interdum posuere. Posuere urna nec tincidunt praesent semper. Sodales ut eu sem integer vitae. Luctus venenatis lectus magna fringilla urna porttitor rhoncus dolor. </p>
+                        </div>
+                    </div>
+                </div>
+                <div className="careers-container">
+                    <h2>Careers</h2>
+                    <div className="why">
+                        <h3>Why Creese Property?</h3>
+                        <p>At Propell, our thriving culture and close-knit community set us apart in the realm of property investment. We believe in fostering an environment where every team member is not just an employee but a valued member of our family. Operating within an open plan office, we embrace a flat management system that encourages collaboration and ensures that no one’s voice goes unheard. With determination as our driving force, we are motivated by the pursuit of excellence and boast a great work ethic that permeates every aspect of our professional lives.</p>
+                    </div>
+                    <div className="what-who">
+                        <div className="who">
+                            <h3>Who We Are?</h3>
+                            <p>
+                                Every day brings new challenges and opportunities, making each moment unique and exciting. As a family-owned business, we take pride in creating an atmosphere where no one goes unnoticed; your contributions are acknowledged, celebrated, and integral to our collective success.
+                                <br /><br />
+                                Join us at Propell, where our commitment to a vibrant culture, unwavering determination, and a sense of community makes us the ideal workplace for those who seek more than just a job – a fulfilling career with a purpose.
+                            </p>
+                        </div>
+                        <div className="what">
+                            <h3>What We Do?</h3>
+                            <p>Propelled by determination, our team is united by a shared motivation and an unwavering work ethic, ensuring that every project is approached with vigour and dedication. Our daily endeavours are marked by diversity and innovation, ensuring that each day presents fresh challenges and opportunities.</p>
                         </div>
                     </div>
                 </div>
