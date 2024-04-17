2026070f997b69447d471af27f25a7eff5cf2061
OriyanPropellProperty <oriyan@propellproperty.com.au>
2024-04-17 12:47:41 +1000

    home page responsive - completed

--- src/components/AutoPlay.jsx
+++ src/components/AutoPlay.jsx
@@ -45,14 +45,14 @@ function AutoPlay() {
             {
                 breakpoint: 1024,
                 settings: {
-                    slidesToShow: 4,
+                    slidesToShow: 3,
                     slidesToScroll: 1,
                 },
             },
             {
                 breakpoint: 600,
                 settings: {
-                    slidesToShow: 3,
+                    slidesToShow: 2,
                     slidesToScroll: 1,
                 },
             },
