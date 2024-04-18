bac7755cc3222b115c0452e18cad4b19ebbc324c
OriyanPropellProperty <oriyan@propellproperty.com.au>
2024-04-18 13:55:50 +1000

    home page adjustments

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -34,29 +34,29 @@ function HomePage() {
                     <video className="header-video" src={videoBg} autoPlay loop muted />
                     <div className="overlay-home"></div>
                 </div>
+                <AutoPlay />
                 <div className="text-home">
-                    <p>Trust our experienced team at Creese Property to secure the right property for you</p>
+                    <p>Trust our experienced team at <span>Creese Property</span> to secure the right property for you</p>
                 </div>
                 <div className="slider-container">
                     <Slider />
                 </div>
-                <AutoPlay />
                 <div className="divider-section">
                     <h2>The <span>Creese Property</span> Promise</h2>
                     <div className="services">
                         <div className="service">
-                            <img src="src/assets/1 copy.png" height={100} alt="" />
+                            <img src="src/assets/1 copy 2.png" height={100} alt="" />
                             <h3>Experience</h3>
                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Purus semper eget duis at tellus at urna condimentum mattis.</p>
                         </div>
                         
                         <div className="service">
-                            <img src="src/assets/2 copy.png" height={100} alt="" />
+                            <img src="src/assets/2 copy 2.png" height={100} alt="" />
                             <h3>Off Market Deals</h3>
                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Purus semper eget duis at tellus at urna condimentum mattis.</p>
                         </div>
                         <div className="service">
-                            <img src="src/assets/3 copy.png" height={100} alt="" />
+                            <img src="src/assets/3 copy 2.png" height={100} alt="" />
                             <h3>Specialisation</h3>
                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Purus semper eget duis at tellus at urna condimentum mattis.</p>
                         </div>
