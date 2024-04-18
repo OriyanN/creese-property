3ccf01bfc09d59e2e8938d22e7cd76213242db7b
OriyanPropellProperty <oriyan@propellproperty.com.au>
2024-04-18 16:56:48 +1000

    faqs section initially closed

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -5,58 +5,58 @@ import './Accordion.css';
 import FAQS from './FAQS';
 
 function Accordion () {
-    const [faqs, setFaqs] = useState([
-        {
-          question: "How many programmers does it take to screw a lightbulb?",
-          answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pharetra lorem eu dolor rhoncus, at scelerisque ligula gravida. Sed porta id mi sit amet convallis. Etiam iaculis massa sit amet lacus blandit sodales. Nulla ultrices velit a diam placerat congue. Pellentesque iaculis, ipsum quis eleifend dapibus, est dui eleifend ante, quis fermentum mi ligula quis nisl. Ut et ex dui. Integer id venenatis quam.",
-          open: true
-        },
-        {
+  const [faqs, setFaqs] = useState([
+      {
+        question: "How many programmers does it take to screw a lightbulb?",
+        answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pharetra lorem eu dolor rhoncus, at scelerisque ligula gravida. Sed porta id mi sit amet convallis. Etiam iaculis massa sit amet lacus blandit sodales. Nulla ultrices velit a diam placerat congue. Pellentesque iaculis, ipsum quis eleifend dapibus, est dui eleifend ante, quis fermentum mi ligula quis nisl. Ut et ex dui. Integer id venenatis quam.",
+        open: false
+      },
+      {
+        question: "Who is the most awesome person?",
+        answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pharetra lorem eu dolor rhoncus, at scelerisque ligula gravida. Sed porta id mi sit amet convallis. Etiam iaculis massa sit amet lacus blandit sodales. Nulla ultrices velit a diam placerat congue. Pellentesque iaculis, ipsum quis eleifend dapibus, est dui eleifend ante, quis fermentum mi ligula quis nisl. Ut et ex dui. Integer id venenatis quam.",
+        open: false
+      },
+      {
+        question: "How many questions does it take to makes a succesful FAQ Page?",
+        answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pharetra lorem eu dolor rhoncus, at scelerisque ligula gravida. Sed porta id mi sit amet convallis. Etiam iaculis massa sit amet lacus blandit sodales. Nulla ultrices velit a diam placerat congue. Pellentesque iaculis, ipsum quis eleifend dapibus, est dui eleifend ante, quis fermentum mi ligula quis nisl. Ut et ex dui. Integer id venenatis quam.",
+        open: false
+      },
+      {
           question: "Who is the most awesome person?",
           answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pharetra lorem eu dolor rhoncus, at scelerisque ligula gravida. Sed porta id mi sit amet convallis. Etiam iaculis massa sit amet lacus blandit sodales. Nulla ultrices velit a diam placerat congue. Pellentesque iaculis, ipsum quis eleifend dapibus, est dui eleifend ante, quis fermentum mi ligula quis nisl. Ut et ex dui. Integer id venenatis quam.",
           open: false
-        },
-        {
+      },
+      {
           question: "How many questions does it take to makes a succesful FAQ Page?",
           answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pharetra lorem eu dolor rhoncus, at scelerisque ligula gravida. Sed porta id mi sit amet convallis. Etiam iaculis massa sit amet lacus blandit sodales. Nulla ultrices velit a diam placerat congue. Pellentesque iaculis, ipsum quis eleifend dapibus, est dui eleifend ante, quis fermentum mi ligula quis nisl. Ut et ex dui. Integer id venenatis quam.",
           open: false
-        },
-        {
-            question: "Who is the most awesome person?",
-            answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pharetra lorem eu dolor rhoncus, at scelerisque ligula gravida. Sed porta id mi sit amet convallis. Etiam iaculis massa sit amet lacus blandit sodales. Nulla ultrices velit a diam placerat congue. Pellentesque iaculis, ipsum quis eleifend dapibus, est dui eleifend ante, quis fermentum mi ligula quis nisl. Ut et ex dui. Integer id venenatis quam.",
-            open: false
-        },
-        {
-            question: "How many questions does it take to makes a succesful FAQ Page?",
-            answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pharetra lorem eu dolor rhoncus, at scelerisque ligula gravida. Sed porta id mi sit amet convallis. Etiam iaculis massa sit amet lacus blandit sodales. Nulla ultrices velit a diam placerat congue. Pellentesque iaculis, ipsum quis eleifend dapibus, est dui eleifend ante, quis fermentum mi ligula quis nisl. Ut et ex dui. Integer id venenatis quam.",
-            open: false
-        }
-    ]);
-    
-    const toggleFAQ = index => {
-        setFaqs(
-          faqs.map((faq, i) => {
-            if (i === index) {
-              faq.open = !faq.open;
-            } else {
-              faq.open = false;
-            }
-    
-            return faq;
-          })
-        );
-    };
-    
-    return (
-        <div className="accordion">
-            <h2>FAQ'S</h2>
-            <div className="faqs">
-                {faqs.map((faq, index) => (
-                <FAQS faq={faq} index={index} key={index} toggleFAQ={toggleFAQ} />
-                ))}
-            </div>
-        </div>
-    );
+      }
+  ]);
+  
+  const toggleFAQ = index => {
+      setFaqs(
+        faqs.map((faq, i) => {
+          if (i === index) {
+            faq.open = !faq.open;
+          } else {
+            faq.open = false;
+          }
+  
+          return faq;
+        })
+      );
+  };
+  
+  return (
+      <div className="accordion">
+          <h2>FAQ'S</h2>
+          <div className="faqs">
+              {faqs.map((faq, index) => (
+              <FAQS faq={faq} index={index} key={index} toggleFAQ={toggleFAQ} />
+              ))}
+          </div>
+      </div>
+  );
 }
     
 
