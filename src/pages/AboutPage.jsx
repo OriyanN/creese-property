cc887b0c6f8666516c1dbd09fa94c1aacb2af9be
OriyanPropellProperty <oriyan@propellproperty.com.au>
2024-05-09 13:52:16 +1000

    removal of careers section from the about page

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -30,7 +30,7 @@ function AboutPage() {
                         <img className="about-image" src="src/assets/Lachie Black.png" alt="Creese Property About Us" />
                     </div>
                     <div className="about-text">
-                        <h2>Lorem Ipsum</h2>
+                        <h2>Our History</h2>
                         <p>
                             Creese Property Pty Ltd emerges as a tribute to the enduring legacy of its founder's lineage, with Managing Director Lachlan Creese dedicating the company to the memory of his late father, David Creese. David was a pioneering figure in the real estate industry, having established Creese Property Consultants on the 15th of May 1981. His firm became a cornerstone in the Southern Tasmanian Commercial Management sector, extending its expertise to encompass residential and commercial development consultancy. Under David's visionary leadership, Creese Property Consultants became instrumental in the development of what is now known as Rosny Park, a key commercial hub on Hobart’s Eastern shore. David’s contributions to real estate also include the establishment of 'Creese Drive' in Richmond, Tasmania, a testament to his success in private residential development that resulted in the creation of over 100 additional lots, predominantly along Cosgrove Drive. It is in this vibrant community that Lachlan Creese spent his formative years, growing up in the family home and gaining firsthand experience in the construction industry as he accompanied his father to numerous sites. David Creese personally took part in the construction and sale of more than 30 houses, instilling in Lachlan a profound understanding and passion for the real estate sector.
                             <br />
@@ -65,27 +65,6 @@ function AboutPage() {
                                 <h4>Property Manager</h4>
                             </div>
                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Purus semper eget duis at tellus at urna condimentum mattis. Etiam non quam lacus suspendisse faucibus. Est ultricies integer quis auctor elit. Mauris commodo quis imperdiet massa tincidunt nunc pulvinar sapien et. Non quam lacus suspendisse faucibus interdum posuere. Posuere urna nec tincidunt praesent semper. Sodales ut eu sem integer vitae. Luctus venenatis lectus magna fringilla urna porttitor rhoncus dolor. </p>
-                        </div>
-                    </div>
-                </div>
-                <div className="careers-container">
-                    <h2>Careers</h2>
-                    <div className="why">
-                        <h3>Why Creese Property?</h3>
-                        <p>At Propell, our thriving culture and close-knit community set us apart in the realm of property investment. We believe in fostering an environment where every team member is not just an employee but a valued member of our family. Operating within an open plan office, we embrace a flat management system that encourages collaboration and ensures that no one’s voice goes unheard. With determination as our driving force, we are motivated by the pursuit of excellence and boast a great work ethic that permeates every aspect of our professional lives.</p>
-                    </div>
-                    <div className="what-who">
-                        <div className="who">
-                            <h3>Who We Are?</h3>
-                            <p>
-                                Every day brings new challenges and opportunities, making each moment unique and exciting. As a family-owned business, we take pride in creating an atmosphere where no one goes unnoticed; your contributions are acknowledged, celebrated, and integral to our collective success.
-                                <br /><br />
-                                Join us at Propell, where our commitment to a vibrant culture, unwavering determination, and a sense of community makes us the ideal workplace for those who seek more than just a job – a fulfilling career with a purpose.
-                            </p>
-                        </div>
-                        <div className="what">
-                            <h3>What We Do?</h3>
-                            <p>Propelled by determination, our team is united by a shared motivation and an unwavering work ethic, ensuring that every project is approached with vigour and dedication. Our daily endeavours are marked by diversity and innovation, ensuring that each day presents fresh challenges and opportunities.</p>
                         </div>
                     </div>
                 </div>
