09ca1a32c25abae81b2b4defae7cc1701f3e676a
OriyanPropellProperty <oriyan@propellproperty.com.au>
2024-05-24 09:07:59 +1000

    services page main change

--- src/pages/ServicesPage.jsx
+++ src/pages/ServicesPage.jsx
@@ -4,6 +4,7 @@ import { Helmet } from 'react-helmet-async';
 import Footer from '../components/Footer';
 import "./ServicePage.css"
 import ScrollToTop from '../components/ScrollToTop';
+import ContactForm from '../components/ContactForm';
 
 function ServicePage() {
 
@@ -46,33 +47,36 @@ function ServicePage() {
                 </script>
             </Helmet>
             <ScrollToTop />
-            <div>
+            <div className='services-section'>
                 <div className="contact-about-header">
                     <img src="src/assets/service-image.jpg" alt="Creese Property Services" />
                     <h1>Services</h1>
                     <div className="overlay"></div>
                 </div>
                 <div className="services-container">
-                    <h2>Services</h2>
                     <div className="services">
                         <div className="service">
-                            <img src="src/assets/1.png" height={100} alt="Creese Property Tailored Solutions" />
-                            <h3>Tailored Solutions</h3>
+                            <img src="src/assets/Residential Property Management.png" height={100} alt="Creese Property Tailored Solutions" />
+                            <h3>Residential Property Management</h3>
                             <p>We understand that every property owner and tenant has their unique needs. That’s why we offer customised management plans to suit your specific requirements.</p>
                         </div>
                         
                         <div className="service">
-                            <img src="src/assets/2.png" height={100} alt="Creese Property Expert Advice" />
-                            <h3>Expert Advice</h3>
+                            <img src="src/assets/Buyers Agency.png" height={100} alt="Creese Property Expert Advice" />
+                            <h3>Buyers Agency</h3>
                             <p>Our team stays aware of the latest market trends and regulations to provide you with informed advice, helping you make the best decisions for your property.</p>
                         </div>
                         <div className="service">
-                            <img src="src/assets/3.png" height={100} alt="Creese Property Community Focus" />
-                            <h3>Community Focus</h3>
+                            <img src="src/assets/Property Consultancy.png" height={100} alt="Creese Property Community Focus" />
+                            <h3>Property Consultancy</h3>
                             <p>We’re not just managing properties, we’re building communities. Creese Property is committed to creating vibrant living spaces where residents can feel at home.</p>
                         </div>
                     </div>
                 </div>
+                <div className="parallax-section">
+                    <div className="overlay-content"></div>
+                </div>
+                <ContactForm />
                 <Footer />
             </div>
         </>
