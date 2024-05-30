7c28453159b8ca3e38fc649d0221b4a7120e119c
OriyanPropellProperty <oriyan@propellproperty.com.au>
2024-05-30 14:19:01 +1000

    removed properties

--- src/pages/GoldCoast/GoldCoastPropertiesData.js
+++ src/pages/GoldCoast/GoldCoastPropertiesData.js
@@ -1,7 +1,5 @@
 import * as MilanCourt9 from '../../assets/GoldCoast/9-milan-court';
 import * as BrindisiAvenue17 from '../../assets/GoldCoast/17-brindisi-ave';
-import * as AvalineEstate170 from '../../assets/GoldCoast/170-avaline-estate';
-import * as TysonStreet9 from '../../assets/GoldCoast/832-9-tyson-street';
 import * as MilanCourt7 from '../../assets/GoldCoast/7-milan-court';
 import * as NorthcliffeTerrace12_4 from '../../assets/GoldCoast/12-4-6-northcliffe-terrace';
 
@@ -44,32 +42,6 @@ const GoldCoastPropertiesData = [
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sodales ut eu sem integer vitae justo eget magna fermentum. Metus dictum at tempor commodo ullamcorper a lacus. Sem nulla pharetra diam sit amet nisl suscipit adipiscing. Etiam tempor orci eu lobortis elementum nibh tellus. Diam maecenas sed enim ut sem viverra aliquet eget. Proin libero nunc consequat interdum varius sit. Sit amet risus nullam eget. Diam volutpat commodo sed egestas egestas fringilla phasellus. Eu nisl nunc mi ipsum. Eget mi proin sed libero enim sed faucibus turpis in. Id semper risus in hendrerit gravida. Sed adipiscing diam donec adipiscing tristique. Convallis convallis tellus id interdum. In vitae turpis massa sed elementum tempus egestas sed. Feugiat nisl pretium fusce id. Donec et odio pellentesque diam volutpat commodo sed egestas egestas. Fringilla ut morbi tincidunt augue interdum.',
     lat: -28.012240,
     lng: 153.425910
-  },
-  {
-    id: '170-avaline-estate',
-    images: Object.values(AvalineEstate170),
-    address: '170 Avaline Estate, Burpengary East',
-    price: 1200,
-    beds: 4,
-    baths: 2,
-    car: 2,
-    features: ['Air conditioning', 'Built-in wardrobes', 'Ensuite', 'Study', 'Dishwasher', 'Deck', 'Balcony', 'Water tank', 'Outdoor area', 'Solar panels'],
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sodales ut eu sem integer vitae justo eget magna fermentum. Metus dictum at tempor commodo ullamcorper a lacus. Sem nulla pharetra diam sit amet nisl suscipit adipiscing. Etiam tempor orci eu lobortis elementum nibh tellus. Diam maecenas sed enim ut sem viverra aliquet eget. Proin libero nunc consequat interdum varius sit. Sit amet risus nullam eget. Diam volutpat commodo sed egestas egestas fringilla phasellus. Eu nisl nunc mi ipsum. Eget mi proin sed libero enim sed faucibus turpis in. Id semper risus in hendrerit gravida. Sed adipiscing diam donec adipiscing tristique. Convallis convallis tellus id interdum. In vitae turpis massa sed elementum tempus egestas sed. Feugiat nisl pretium fusce id. Donec et odio pellentesque diam volutpat commodo sed egestas egestas. Fringilla ut morbi tincidunt augue interdum.',
-    lat: -27.122400,
-    lng: 152.977990
-  },
-  {
-    id: '832-9-tyson-street',
-    images: Object.values(TysonStreet9),
-    address: '832/9 Tyson Street, Whiterock Ripley',
-    price: 1300,
-    beds: 4,
-    baths: 2,
-    car: 2,
-    features: ['Air conditioning', 'Built-in wardrobes', 'Ensuite', 'Study', 'Deck', 'Balcony', 'Water tank', 'Outdoor area', 'Solar panels'],
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sodales ut eu sem integer vitae justo eget magna fermentum. Metus dictum at tempor commodo ullamcorper a lacus. Sem nulla pharetra diam sit amet nisl suscipit adipiscing. Etiam tempor orci eu lobortis elementum nibh tellus. Diam maecenas sed enim ut sem viverra aliquet eget. Proin libero nunc consequat interdum varius sit. Sit amet risus nullam eget. Diam volutpat commodo sed egestas egestas fringilla phasellus. Eu nisl nunc mi ipsum. Eget mi proin sed libero enim sed faucibus turpis in. Id semper risus in hendrerit gravida. Sed adipiscing diam donec adipiscing tristique. Convallis convallis tellus id interdum. In vitae turpis massa sed elementum tempus egestas sed. Feugiat nisl pretium fusce id. Donec et odio pellentesque diam volutpat commodo sed egestas egestas. Fringilla ut morbi tincidunt augue interdum.',
-    lat: -27.416270,
-    lng: 153.032310
   },
   {
     id: '7-milan-court',
