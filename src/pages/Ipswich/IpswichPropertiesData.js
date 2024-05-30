f55c4074e29ab1b69b7fa18bdabb3ccca6f1234d
OriyanPropellProperty <oriyan@propellproperty.com.au>
2024-05-31 08:53:31 +1000

    features changed

--- src/pages/Ipswich/IpswichPropertiesData.js
+++ src/pages/Ipswich/IpswichPropertiesData.js
@@ -9,7 +9,7 @@ const IpswichPropertiesData = [
     beds: 4,
     baths: 2,
     car: 2,
-    features: ['Air conditioning', 'Built-in wardrobes', 'Ensuite', 'Study', 'Deck', 'Balcony', 'Water tank', 'Outdoor area', 'Solar panels'],
+    features: ['Air conditioning', 'Roller Blinds', 'Mirrored Robes', 'Quality Carpets', 'Deck', 'Driveway', 'Wall-hung Vanities', 'Outdoor area', 'Stone Benchtops'],
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sodales ut eu sem integer vitae justo eget magna fermentum. Metus dictum at tempor commodo ullamcorper a lacus. Sem nulla pharetra diam sit amet nisl suscipit adipiscing. Etiam tempor orci eu lobortis elementum nibh tellus. Diam maecenas sed enim ut sem viverra aliquet eget. Proin libero nunc consequat interdum varius sit. Sit amet risus nullam eget. Diam volutpat commodo sed egestas egestas fringilla phasellus. Eu nisl nunc mi ipsum. Eget mi proin sed libero enim sed faucibus turpis in. Id semper risus in hendrerit gravida. Sed adipiscing diam donec adipiscing tristique. Convallis convallis tellus id interdum. In vitae turpis massa sed elementum tempus egestas sed. Feugiat nisl pretium fusce id. Donec et odio pellentesque diam volutpat commodo sed egestas egestas. Fringilla ut morbi tincidunt augue interdum.',
     lat: -27.416270,
     lng: 153.032310
