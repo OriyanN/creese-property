fa300499754e5ef07b754e457e939ee7cc12ba63
OriyanPropellProperty <oriyan@propellproperty.com.au>
2024-06-17 13:08:26 +1000

    content added

--- src/pages/AssistantPropertyManager.jsx
+++ src/pages/AssistantPropertyManager.jsx
@@ -24,34 +24,40 @@ function AssistantPropertyManagerPage() {
                     <div className="overlay"></div>
                 </div>
                 <div className="description-section">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus enim quidem hic aut quo nam, dolore nostrum, ullam maxime cum ex ut temporibus recusandae aspernatur! Nulla ut quod neque ratione! Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus enim quidem hic aut quo nam, dolore nostrum, ullam maxime cum ex ut temporibus recusandae aspernatur! Nulla ut quod neque ratione! Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus enim quidem hic aut quo nam, dolore nostrum, ullam maxime cum ex ut temporibus recusandae aspernatur! Nulla ut quod neque ratione! Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus enim quidem hic aut quo nam, dolore nostrum, ullam maxime cum ex ut temporibus recusandae aspernatur! Nulla ut quod neque ratione!</p>
+                    <p>
+                        Creese Property is a dynamic property management firm with a culture first focus. We believe in changing industry expectation surrounding the level of service that should be expected from landlords and tenants alike. Through innovation and understanding we continue to shape our business to reflect the everchanging requirements and expectations of the industry.
+                    </p>
+                        <br></br>
+                    <p>
+                        As a business we are firm believers in being united, as Together Everyone Achieves More (T.E.A.M.). While this acronym is tacky, it has allowed us to build invaluable trust with our clients to care for their investments as our own. 
+                        We facilitate the realities of life, putting our workplace culture and staff’s individual needs as a top priority; fostering business foundation and workplace environment that they can pursue a lifelong career.
+                    </p>
                     <div className="description-mini-section qualification-section">
+                        <h3>Job Description</h3>
+                        <p>As an Assistant Property Manager, you will work directly with the Senior Property Management team, assisting and learning from them. This will be through general property admin, entry/exit reporting, and processing rental applications. The goal of this role is to be trained up to become a senior property manager with your own portfolio.</p>
+                    </div>
+                    <div className="description-mini-section">
                         <h3>Qualifications and Expertise</h3>
                         <ul>
-                            <li>A minimum of 5 years of experience in an agency setting is essential for this role.</li>
-                            <li>Comprehensive knowledge of PPC/Media and substantial experience in managing advertising campaigns on Google, Meta and Programmatic platforms (such as DV360, Stackadapt, or similar).</li>
-                            <li>Solid understanding of various media platforms including Google Ads, Bing Ads, Facebook & Instagram, LinkedIn, TikTok, and programmatic advertising.</li>
-                            <li>Knowledge of B2B marketing strategies.</li>
-                            <li>Exceptional analytical abilities and a keen insight into important metrics.</li>
-                            <li>Demonstrated capability to effectively convey information to diverse audiences.</li>
-                            <li>The skill to oversee multiple projects concurrently.</li>
-                            <li>Strong communication and teamwork skills are crucial.</li>
-                            <li>A relentless drive to achieve objectives within time and budget constraints.</li>
-                            <li>Valued but not essential: Experience in media planning and purchasing via DSPs.</li>
+                            <li>QLD Real Estate License or Registration.</li>
+                            <li>Previous experience in a similar role.</li>
+                            <li>Strong communication and interpersonal skills.</li>
+                            <li>Excellent organisational and time management abilities.</li>
+                            <li>Attention to detail with accuracy first approach.</li>
+                            <li>Previous experience using PropertyMe (preferred).</li>
                         </ul>
                     </div>
                     <div className="description-mini-section">
                         <h3>What We Offer</h3>
                         <ul>
-                            <li>A minimum of 5 years of experience in an agency setting is essential for this role.</li>
-                            <li>Comprehensive knowledge of PPC/Media and substantial experience in managing advertising campaigns on Google, Meta and Programmatic platforms (such as DV360, Stackadapt, or similar).</li>
-                            <li>Solid understanding of various media platforms including Google Ads, Bing Ads, Facebook & Instagram, LinkedIn, TikTok, and programmatic advertising.</li>
-                            <li>Knowledge of B2B marketing strategies.</li>
+                            <li>Opportunities for professional development and growth.</li>
+                            <li>A supportive and collaborative work environment.</li>
+                            <li>The chance to be immersed in multiple areas of the Property sector.</li>
                         </ul>
                     </div>
                     <div className="description-mini-section">
                         <h3>Salary</h3>
-                        <p>Structured upon experience and capacity of person (negotiable)</p>
+                        <p>Structured upon experience and capacity of person.</p>
                     </div>
                     <div className="description-mini-section">
                         <h3>How to Apply</h3>
