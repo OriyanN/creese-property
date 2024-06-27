5cc88c1e77c2423ecd8cf4b5dfeaa69257ea99f3
OriyanPropellProperty <oriyan@propellproperty.com.au>
2024-06-27 16:00:55 +1000

    index tryout

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -30,7 +30,8 @@ function AboutPage() {
                         "@graph": [
                           {
                             "@type": "BreadcrumbList",
-                            "@id": "https://www.creeseproperty.com/about/#breadcrumb",
+                            // "@id": "https://www.creeseproperty.com/about/#breadcrumb",
+                            "@id": "https://www.creeseproperty.com/#breadcrumb",
                             "itemListElement": [
                               {
                                 "@type": "ListItem",
