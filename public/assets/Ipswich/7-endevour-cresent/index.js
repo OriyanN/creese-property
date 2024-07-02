581668f9667c8201d111f0caa3a61471f98eed25
OriyanPropellProperty <oriyan@propellproperty.com.au>
2024-07-02 12:25:21 +1000

    added a property

---
+++ public/assets/Ipswich/7-endevour-cresent/index.js
@@ -0,0 +1,35 @@
+import image0_1_jpg from './1.jpg';
+import image1_10_jpg from './10.jpg';
+import image2_11_jpg from './11.jpg';
+import image3_12_jpg from './12.jpg';
+import image4_13_jpg from './13.jpg';
+import image5_14_jpg from './14.jpg';
+import image6_15_jpg from './15.jpg';
+import image7_16_jpg from './16.jpg';
+import image8_2_jpg from './2.jpg';
+import image9_3_jpg from './3.jpg';
+import image10_4_jpg from './4.jpg';
+import image11_5_jpg from './5.jpg';
+import image12_6_jpg from './6.jpg';
+import image13_7_jpg from './7.jpg';
+import image14_8_jpg from './8.jpg';
+import image15_9_jpg from './9.jpg';
+
+export {
+  image0_1_jpg,
+  image1_10_jpg,
+  image2_11_jpg,
+  image3_12_jpg,
+  image4_13_jpg,
+  image5_14_jpg,
+  image6_15_jpg,
+  image7_16_jpg,
+  image8_2_jpg,
+  image9_3_jpg,
+  image10_4_jpg,
+  image11_5_jpg,
+  image12_6_jpg,
+  image13_7_jpg,
+  image14_8_jpg,
+  image15_9_jpg
+};
--- src/pages/Ipswich/IpswichPropertiesData.js
+++ src/pages/Ipswich/IpswichPropertiesData.js
@@ -1,4 +1,5 @@
 import * as TysonStreet9 from '../../../public/assets/Ipswich/832-9-tyson-street';
+import * as EndeavourCres7 from '../../../public/assets/Ipswich/7-endevour-cresent';
 
 const IpswichPropertiesData = [
   {
@@ -13,6 +14,19 @@ const IpswichPropertiesData = [
     description: ["New Built 4 Bedroom Home with quality throughout. Deebing Heights is a large suburb encompassing the area from the eponymous hills surrounding Deebing Creek in the north of the suburb through to the lower foothills of the Flinders Peak Group in the south. From the Grampian Hills it is possible, on a clear day, to see the Brisbane skyline and the Pacific Ocean.", "", "Features:", "- 4 Spacious bedrooms", "- 2 Modern bathrooms with Wall hung vanities with above counter basins.", "- 2 Separate living areas for maximum comfort", "- 2 Car garage with ample storage", "- Kitchen with Stainless steel cooker & Gooseneck tap wear", "- Stone bench tops to Kitchen and Laundry","", "Additional Features:", "- blinds throughout", "- Beautifully landscaped gardens", "", "This brand-new home won't last long. Contact us today to arrange a viewing!"],
     lat: -27.416270,
     lng: 153.032310
+  },
+  {
+    id: '7-endevour-cresent',
+    images: Object.values(EndeavourCres7),
+    address: '7 Endeavour Cres, Pallara',
+    price: 835,
+    beds: 4,
+    baths: 2.5,
+    car: 2,
+    features: ['Air conditioning', 'Roller Blinds', 'Quality Carpets', 'Driveway', 'Outdoor area', 'Stone Benchtops', 'Dishwasher', 'Double Garage', 'Built-in wardrobes'],
+    description: ["Discover your dream home at Hideaway Estate! This magnificent 4-bedroom, 2-bathrooms PLUS Powder Room residence offers the perfect blend of luxury and comfort.", "Downstairs:", "- Entry", "-Spacious garage", "- Family room", "- Separate dining room", "- Modern kitchen with Smeg dishwasher, Smeg Oven and Cooktop", "- Walk In Pantry", "- Laundry", "- Powder room with Frameless Mirrors", "- Alfresco area for outdoor entertaining or for a relaxing weekend", "Upstairs:", "- 4 bedrooms, including:", "- Master bedroom with walk-in robe and ensuite", "- Frameless Mirrors to Ensuite and Main Bathroom", "- Sitting area at the top of the stairs", "- Main Bathroom with Bath", "- Separate toilet", "- Study nook", "Additional Features:", "- Ducted air conditioning throughout for year-round comfort", "Brand new community, close to Pallara Childcare, Pallara Early Learning Centre, Pallara Park, Pallara State School, St John's Anglican College, Forest Lake State High School, Pallara Shopping Village with Coles, Public Transport. The suburb features local parks with playgrounds, BBQ areas, and sports fields, providing opportunities for relaxation and family gatherings. Additionally, nearby natural reserves such as Karawatha Forest offer residents walking trails and wildlife viewing experiences", "Approximately 30 minutes to Brisbane CBD and 55 minutes to Surfers Paradise", "Don’t miss the opportunity to make it yours! Contact us today to arrange a viewing."],
+    lat: -27.619210,
+    lng: 152.996900
   },
 ];
   
