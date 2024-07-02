2a3d19b4f1ee2e85cbd38e289877cd1f9923bd7b
OriyanPropellProperty <oriyan@propellproperty.com.au>
2024-07-02 12:25:36 +1000

    fix to node generator

--- public/assets/index.js
+++ public/assets/index.js
@@ -1,111 +1,91 @@
 import image0_Buyers_Agency_png from './Buyers Agency.png';
 import image1_CP_(2)_png from './CP (2).png';
-import image2_CP_Icon_White_png from './CP Icon White.png';
-import image3_CP_Icon_png from './CP Icon.png';
-import image4_CP_Transparent_Grey_png from './CP Transparent Grey.png';
-import image5_CP_Transparent_Text1_png from './CP Transparent Text1.png';
-import image6_CP_Transparent(1)_png from './CP Transparent(1).png';
-import image7_CP_Transparent(2)_png from './CP Transparent(2).png';
-import image8_CP_Transparent_png from './CP Transparent.png';
-import image9_CP_WHITE_GREY_png from './CP WHITE GREY.png';
-import image10_Creese_Property_Favicon_White_png from './Creese Property Favicon White.png';
-import image11_Creese_Property_Logo__White_White_png from './Creese Property Logo- White White.png';
-import image12_Creese_Property_Logo__White_White2_png from './Creese Property Logo- White White2.png';
-import image13_Creese_Property_Logo__White_White3_png from './Creese Property Logo- White White3.png';
-import image14_Creese_Property_Logo__White_White4_png from './Creese Property Logo- White White4.png';
-import image15_Creese_Property_Logo_Beige_png from './Creese Property Logo-Beige.png';
-import image16_Kym_(1)_png from './Kym (1).png';
-import image17_Lachie_Black_png from './Lachie Black.png';
-import image18_Lachie_White_png from './Lachie White.png';
-import image19_Property_Consultancy_png from './Property Consultancy.png';
-import image20_Residential_Property_Management_png from './Residential Property Management.png';
-import image21_about_image_jpg from './about-image.jpg';
-import image22_contact_image_jpg from './contact-image.jpg';
-import image23_facebook_blue_png from './facebook-blue.png';
-import image24_facebook_white_png from './facebook-white.png';
-import image25_gold_coast_jpg from './gold-coast.jpg';
-import image26_gold_coast1_jpg from './gold-coast1.jpg';
-import image27_gold_coast3_jpg from './gold-coast3.jpg';
-import image28_gold_coast3_png from './gold-coast3.png';
-import image29_gold_coast4_jpg from './gold-coast4.jpg';
-import image30_gold_coast4_png from './gold-coast4.png';
-import image31_home_page_gold_coast_jpg from './home-page-gold-coast.jpg';
-import image32_icons8_facebook_50_copy_png from './icons8-facebook-50 copy.png';
-import image33_icons8_facebook_50_png from './icons8-facebook-50.png';
-import image34_icons8_forward_100_png from './icons8-forward-100.png';
-import image35_icons8_instagram_48_(1)_png from './icons8-instagram-48 (1).png';
-import image36_icons8_instagram_48_png from './icons8-instagram-48.png';
-import image37_icons8_instagram_50_(1)_png from './icons8-instagram-50 (1).png';
-import image38_icons8_instagram_50_png from './icons8-instagram-50.png';
-import image39_icons8_linkedin_48_(1)_png from './icons8-linkedin-48 (1).png';
-import image40_icons8_linkedin_48_png from './icons8-linkedin-48.png';
-import image41_icons8_linkedin_50_(1)_png from './icons8-linkedin-50 (1).png';
-import image42_icons8_linkedin_50_png from './icons8-linkedin-50.png';
-import image43_left_arrow_png from './left-arrow.png';
-import image44_linkedin_blue_png from './linkedin-blue.png';
-import image45_linkedin_white_png from './linkedin-white.png';
-import image46_portfolio_image_jpg from './portfolio-image.jpg';
-import image47_right_arrow_home_page3_png from './right-arrow-home-page3.png';
-import image48_right_arrow_png from './right-arrow.png';
-import image49_service_image_jpg from './service-image.jpg';
-import image50_service1_png from './service1.png';
-import image51_service2_png from './service2.png';
-import image52_service3_png from './service3.png';
-import image53_services_middle_image_jpg from './services-middle-image.jpg';
+import image2_CP_Transparent(2)_png from './CP Transparent(2).png';
+import image3_CPIconWhite_png from './CPIconWhite.png';
+import image4_CreesePropertyInitialImage_png from './CreesePropertyInitialImage.png';
+import image5_Creese_Property_Logo_White_White_png from './Creese_Property_Logo-White_White.png';
+import image6_David_Creese_jpg from './David Creese.jpg';
+import image7_David_Creese_png from './David Creese.png';
+import image8_Kirby_Otto_Black_Smaller_jpg from './Kirby Otto Black Smaller.jpg';
+import image9_Kym_(1)_png from './Kym (1).png';
+import image10_Kym_jpg from './Kym.jpg';
+import image11_Lachie_Black_png from './Lachie Black.png';
+import image12_Lachie_Suit_White_jpg from './Lachie Suit White.jpg';
+import image13_Lachie_Sweater_jpg from './Lachie Sweater.jpg';
+import image14_Lachie_White_png from './Lachie White.png';
+import image15_Property_Consultancy_png from './Property Consultancy.png';
+import image16_Residential_Property_Management_png from './Residential Property Management.png';
+import image17_SocialSharingImage_png from './SocialSharingImage.png';
+import image18_about_image_jpg from './about-image.jpg';
+import image19_assistant_property_manager_jpg from './assistant-property-manager.jpg';
+import image20_careers_image_jpg from './careers-image.jpg';
+import image21_contact_image_jpg from './contact-image.jpg';
+import image22_facebook_blue_png from './facebook-blue.png';
+import image23_facebook_white_png from './facebook-white.png';
+import image24_home_page_gold_coast_jpg from './home-page-gold-coast.jpg';
+import image25_icons8_arrow_50_png from './icons8-arrow-50.png';
+import image26_icons8_forward_100_png from './icons8-forward-100.png';
+import image27_icons8_instagram_48_(1)_png from './icons8-instagram-48 (1).png';
+import image28_icons8_instagram_48_png from './icons8-instagram-48.png';
+import image29_icons8_linkedin_48_(1)_png from './icons8-linkedin-48 (1).png';
+import image30_icons8_linkedin_48_png from './icons8-linkedin-48.png';
+import image31_leasing_image_jpg from './leasing-image.jpg';
+import image32_left_arrow_png from './left-arrow.png';
+import image33_portfolio_image_jpg from './portfolio-image.jpg';
+import image34_process1_png from './process1.png';
+import image35_process2_png from './process2.png';
+import image36_process3_png from './process3.png';
+import image37_right_arrow_home_page3_png from './right-arrow-home-page3.png';
+import image38_right_arrow_png from './right-arrow.png';
+import image39_service_image_jpg from './service-image.jpg';
+import image40_service1_svg from './service1.svg';
+import image41_service2_svg from './service2.svg';
+import image42_service3_svg from './service3.svg';
+import image43_services_middle_image_jpg from './services-middle-image.jpg';
 
 export {
   image0_Buyers_Agency_png,
   image1_CP_(2)_png,
-  image2_CP_Icon_White_png,
-  image3_CP_Icon_png,
-  image4_CP_Transparent_Grey_png,
-  image5_CP_Transparent_Text1_png,
-  image6_CP_Transparent(1)_png,
-  image7_CP_Transparent(2)_png,
-  image8_CP_Transparent_png,
-  image9_CP_WHITE_GREY_png,
-  image10_Creese_Property_Favicon_White_png,
-  image11_Creese_Property_Logo__White_White_png,
-  image12_Creese_Property_Logo__White_White2_png,
-  image13_Creese_Property_Logo__White_White3_png,
-  image14_Creese_Property_Logo__White_White4_png,
-  image15_Creese_Property_Logo_Beige_png,
-  image16_Kym_(1)_png,
-  image17_Lachie_Black_png,
-  image18_Lachie_White_png,
-  image19_Property_Consultancy_png,
-  image20_Residential_Property_Management_png,
-  image21_about_image_jpg,
-  image22_contact_image_jpg,
-  image23_facebook_blue_png,
-  image24_facebook_white_png,
-  image25_gold_coast_jpg,
-  image26_gold_coast1_jpg,
-  image27_gold_coast3_jpg,
-  image28_gold_coast3_png,
-  image29_gold_coast4_jpg,
-  image30_gold_coast4_png,
-  image31_home_page_gold_coast_jpg,
-  image32_icons8_facebook_50_copy_png,
-  image33_icons8_facebook_50_png,
-  image34_icons8_forward_100_png,
-  image35_icons8_instagram_48_(1)_png,
-  image36_icons8_instagram_48_png,
-  image37_icons8_instagram_50_(1)_png,
-  image38_icons8_instagram_50_png,
-  image39_icons8_linkedin_48_(1)_png,
-  image40_icons8_linkedin_48_png,
-  image41_icons8_linkedin_50_(1)_png,
-  image42_icons8_linkedin_50_png,
-  image43_left_arrow_png,
-  image44_linkedin_blue_png,
-  image45_linkedin_white_png,
-  image46_portfolio_image_jpg,
-  image47_right_arrow_home_page3_png,
-  image48_right_arrow_png,
-  image49_service_image_jpg,
-  image50_service1_png,
-  image51_service2_png,
-  image52_service3_png,
-  image53_services_middle_image_jpg
+  image2_CP_Transparent(2)_png,
+  image3_CPIconWhite_png,
+  image4_CreesePropertyInitialImage_png,
+  image5_Creese_Property_Logo_White_White_png,
+  image6_David_Creese_jpg,
+  image7_David_Creese_png,
+  image8_Kirby_Otto_Black_Smaller_jpg,
+  image9_Kym_(1)_png,
+  image10_Kym_jpg,
+  image11_Lachie_Black_png,
+  image12_Lachie_Suit_White_jpg,
+  image13_Lachie_Sweater_jpg,
+  image14_Lachie_White_png,
+  image15_Property_Consultancy_png,
+  image16_Residential_Property_Management_png,
+  image17_SocialSharingImage_png,
+  image18_about_image_jpg,
+  image19_assistant_property_manager_jpg,
+  image20_careers_image_jpg,
+  image21_contact_image_jpg,
+  image22_facebook_blue_png,
+  image23_facebook_white_png,
+  image24_home_page_gold_coast_jpg,
+  image25_icons8_arrow_50_png,
+  image26_icons8_forward_100_png,
+  image27_icons8_instagram_48_(1)_png,
+  image28_icons8_instagram_48_png,
+  image29_icons8_linkedin_48_(1)_png,
+  image30_icons8_linkedin_48_png,
+  image31_leasing_image_jpg,
+  image32_left_arrow_png,
+  image33_portfolio_image_jpg,
+  image34_process1_png,
+  image35_process2_png,
+  image36_process3_png,
+  image37_right_arrow_home_page3_png,
+  image38_right_arrow_png,
+  image39_service_image_jpg,
+  image40_service1_svg,
+  image41_service2_svg,
+  image42_service3_svg,
+  image43_services_middle_image_jpg
 };
