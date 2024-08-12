6505990bd669f89888948dd4aa2760fa9b5216ca
OriyanPropellProperty <oriyan@propellproperty.com.au>
2024-08-12 10:11:35 +1000

    created an error page

--- src/main.jsx
+++ src/main.jsx
@@ -70,7 +70,7 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HelmetProvider>
-      <ErrorBoundary >
+      <ErrorBoundary>
         <Suspense fallback={<Loader />}>
           <RouterProvider router={router}>
             <ScrollToTop />
