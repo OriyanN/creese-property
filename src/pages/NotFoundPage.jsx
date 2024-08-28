557aa9415a1e35f1dcacfe38ff3148327d3648d1
OriyanPropellProperty <oriyan@propellproperty.com.au>
2024-08-28 16:42:32 +1000

    changed the button colours

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -19,7 +19,7 @@ function NotFoundPage() {
             <div className='notfound-background'>
                 <h1 className='notfound-header'>404</h1>
                 <p>Sorry, this page isn’t available. <br/>The link you followed may be broken, <br/>or the page may have been removed.</p>
-                <Link to="/" className='gallery-button' style={{ textDecoration: 'underline' }}>To Creese Property Home</Link>
+                <Link to="/" className='leased-button' style={{ textDecoration: 'underline' }}>To Creese Property Home</Link>
             </div>
         </>
     );
