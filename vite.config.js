778999f8ccd54aa61752f3e34d873cbe76641021
OriyanPropellProperty <oriyan@propellproperty.com.au>
2025-01-13 14:23:15 +1000

    added compression plugin

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import htmlMinifier from 'vite-plugin-html-minifier';
+import viteCompression from 'vite-plugin-compression';
 
 export default defineConfig({
   plugins: [
@@ -13,21 +14,37 @@ export default defineConfig({
       removeStyleLinkTypeAttributes: true,
       useShortDoctype: true,
     }),
+    viteCompression({
+      algorithm: 'brotliCompress', // Use Brotli compression
+      ext: '.br', // File extension for Brotli-compressed files
+      threshold: 10240, // Only compress files larger than 10 KB
+      deleteOriginFile: false, // Keep the original uncompressed files
+    }),
   ],
   build: {
+    cssCodeSplit: true,
     minify: 'terser',
     terserOptions: {
       compress: {
+        unused: true,
         drop_console: true,
       },
     },
     rollupOptions: {
-      external: [
-        // List any external dependencies here if needed
-      ],
-    },
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react') || id.includes('react-dom')) {
+              return 'react-vendor';
+            }
+            return 'vendor';
+          }
+        },
+      },
+    }    
   },
   server: {
-    compression: true, // Enable Brotli or Gzip compression
+    host: '0.0.0.0', // Ensure the server is accessible externally
+    port: 5173, // Explicitly set the port
   },
-});
+}); 
