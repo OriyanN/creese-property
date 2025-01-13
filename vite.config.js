27e2419ef7c709608898534e6c7e425162383d1b
OriyanPropellProperty <oriyan@propellproperty.com.au>
2025-01-13 16:42:29 +1000

    Revert "added compression plugin"
    This reverts commit 778999f8ccd54aa61752f3e34d873cbe76641021.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import htmlMinifier from 'vite-plugin-html-minifier';
-import viteCompression from 'vite-plugin-compression';
 
 export default defineConfig({
   plugins: [
@@ -14,38 +13,21 @@ export default defineConfig({
       removeStyleLinkTypeAttributes: true,
       useShortDoctype: true,
     }),
-    viteCompression({
-      algorithm: 'brotliCompress', // Use Brotli compression
-      ext: '.br', // File extension for Brotli-compressed files
-      threshold: 10240, // Only compress files larger than 10 KB
-      deleteOriginFile: false, // Keep the original uncompressed files
-    }),
   ],
   build: {
-    sourcemap: true,
-    cssCodeSplit: true,
     minify: 'terser',
     terserOptions: {
       compress: {
-        unused: true,
         drop_console: true,
       },
     },
     rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            if (id.includes('react') || id.includes('react-dom')) {
-              return 'react-vendor';
-            }
-            return 'vendor';
-          }
-        },
-      },
-    }    
+      external: [
+        // List any external dependencies here if needed
+      ],
+    },
   },
   server: {
-    host: '0.0.0.0', // Ensure the server is accessible externally
-    port: 5173, // Explicitly set the port
+    compression: true, // Enable Brotli or Gzip compression
   },
-}); 
+});
